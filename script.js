aa715cfccbedb3e59791dbe6142e234ac0f5066a
Gab-LRL <149244493+Gab-LRL@users.noreply.github.com>
2024-06-20 00:11:23 -0400

    Ya se concatena

--- script.js
+++ script.js
@@ -42,32 +42,41 @@ class TransaccionBase {
         console.log('Su venta ha sido cancelada exitosamente.');
       } else {
         console.log(
-          'El número de venta no está asociado a ninguna venta existente.'
+          `El número de venta no está asociado a ninguna venta existente.`
         );
       }
     }
+    return;
   }
   
   function mostrarVentas() {
     if (ventas.length === 0) {
-      console.log('No hay ventas registradas.');
+      console.log(`No hay ventas registradas.`);
     } else {
-      console.log('Ventas Registradas:');
+      console.log(`Ventas Registradas:`);
       for (let i = 0; i < ventas.length; i++) {
         const venta = ventas[i];
-        console.log( 'Venta #: $ {i + 1} Producto: ${venta.producto}, Cantidad: ${venta.cantidad}, Precio: ${venta.precio}'
-        );
+        console.log( `Venta #: ${i + 1} Producto: ${venta.producto}, Cantidad: ${venta.cantidad}, Precio: ${venta.precio}`);
       }
     }
+    return;
   }
   function menu() {
-    let opcion = '';
+    let opcion = null ;
+
     do {
-      opcion = prompt( 'Seleccione una opción:\n1. Registrar Venta\n2. Cancelar Venta\n3. Mostrar Ventas\n4. Salir,' );
+      opcion = prompt('Seleccione una opción:\n1. Registrar Venta\n2. Cancelar Venta\n3. Mostrar Ventas\n4. Salir');
   
       switch (opcion) {
         case '1':
-          registrarVenta();
+let c1;
+do{
+
+  registrarVenta();
+ c1 = prompt ('Marque 1 si desea agreagar otra venta y 0 en caso contrario');
+
+}while(c1 === 1);
+          
           break;
         case '2':
           cancelarVenta();
